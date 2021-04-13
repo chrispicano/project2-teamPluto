759817fc7be02aed0bc6935655780bdbe8d0a872
chrisPicano <cpicano25@gmail.com>
2021-04-13 15:20:53 -0400

    Fixed bugg with API, search bar still wont work

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,7 @@
+let api_key = "e71ebb6879724f28a83fc531982e1b06";
+// console.log(process.env.API);
+// require('dotenv').config();
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -43,6 +47,62 @@ console.log(name, email, password);
     }
   }
 };
+
+//Test api w searchbar 
+const gamesList = document.getElementById('gamesList');
+const searchBar = document.getElementById('searchBar');
+let rawgGames = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    console.log("searchString",searchString);
+
+    const filteredGames = rawgGames.filter((game) => {
+        return (
+            game.name.toLowerCase().includes(searchString) ||
+            game.genre.toLowerCase().includes(searchString)
+        );
+    });
+    displayGames(filteredGames);
+});
+
+const loadGames = async () => {
+  console.log("load games");
+    try {
+        const res = await fetch('https://api.rawg.io/api/games?key='+api_key+'&dates=2019-09-01,2019-09-30&platforms=18,1,7').then (res => {
+          return res.json()
+        }).then(data =>{
+          console.log(data.results);
+          displayGames(data.results);
+        })
+
+        // rawgGames = await res.json();
+        // displayGames(rawgGames);
+        // console.log(res);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayGames = (game) => {
+    const htmlString = game
+        .map((game) => {
+            return `
+            <li class="game">
+                <h2>${game.name}</h2>
+                <p>Genres: ${game.genres.map(g => g.name)}</p>
+                <img src="${game.background_image}"></img>
+            </li>
+        `;
+        console.log(game);
+        })
+        .join('');
+    gamesList.innerHTML = htmlString;
+};
+
+document
+.querySelector("#searchBar")
+.addEventListener("submit", loadGames());
 
 document
   .querySelector('.login-form')
